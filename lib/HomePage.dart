d812e2fe60dd285d47275862ea621ee8d89fa573
akash <akashothayoth@gmail.com>
2024-03-18 12:25:24 +0530

    hlo

---
+++ lib/HomePage.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:todo_hive/main.dart';
+import 'package:intl/intl.dart';
+
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Box<Todo> todoBox;
+
+  @override
+  void initState(){
+  
+  super.initState();
+  todoBox = Hive.box<Todo>('todo');
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Colors.white,
+        title: Text("Todo List"),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: todoBox.listenable(),
+        builder: (context, Box<Todo> box, _){
+          return ListView.builder(
+            itemCount: box.length,
+            itemBuilder: (context, index) {
+              Todo todo = box.getAt(index)!;
+              return Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: todo.isCompleted ? Colors.white38 : Colors.white,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                
+                child: Dismissible(
+                  key: Key(todo.dateTime.toString()),
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+                    color: Colors.redAccent,
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+
+                    child:Icon(
+                       Icons.delete,
+                    color:Colors.white
+                    )
+                  ),
+                  onDismissed: (direction) {
+                    setState(() {
+                      todo.delete();
+                    });
+                  },
+                  child: ListTile(
+                    title: Text(todo.title),
+                    subtitle: Text(todo.Description),
+                    trailing: Text(
+                      DateFormat.yMMMd().format(todo.dateTime)
+                    ),
+                  
+                    leading: Checkbox(
+                      value: todo.isCompleted,
+                      onChanged: (value){
+                        setState(() {
+                          todo.isCompleted = value!;
+                          todo.save();
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              );
+            }
+          );
+        }
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            _addTodoDialog(context);
+          },
+          child: Icon(Icons.add),
+          ),
+    );
+  }
+  void _addTodoDialog(BuildContext context){
+    TextEditingController tittleController = TextEditingController();
+    TextEditingController discriptionController = TextEditingController();
+
+    showDialog(
+      context: context,
+       builder: (context) => AlertDialog(
+        title: Text("Add Task"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: tittleController,
+              decoration: InputDecoration(labelText: "Title"),
+            ),
+            TextField(
+              controller: discriptionController,
+              decoration: InputDecoration(labelText: "Description"),
+            )
+          ],
+        ),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.pop(context);
+          }, child: Text('Cancel')),
+          TextButton(onPressed: (){
+            _addTodo(tittleController.text, discriptionController.text);
+          }, child: Text('Add'))
+        ],
+       )
+       );
+  }
+  void _addTodo(String title, String Description){
+    if(title.isNotEmpty){
+      todoBox.add(
+        Todo(
+        title:title,
+        Description: Description,
+        dateTime: DateTime.now()
+        )
+      );
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:todo_hive/HomePage.dart';
+
+void main()async{
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoAdapter());
+  await Hive.openBox<Todo>("todo");
+  runApp(MyApp());
+}
+@HiveType(typeId: 0)
+class Todo extends HiveObject {
+  @HiveField(0)
+  late String title;
+
+  @HiveField(1)
+  late String Description;
+
+  @HiveField(2)
+  late bool isCompleted;
+
+  @HiveField(3)
+  late DateTime dateTime;
+
+  Todo({
+    required this.title,
+    required this.Description,
+    required this.dateTime,
+    this.isCompleted = false,
+  });
+  
+}
+class TodoAdapter extends TypeAdapter<Todo>{
+  @override
+   final int typeId = 0;
+
+   @override
+   Todo read(BinaryReader reader){
+    return Todo(
+      title: reader.readString(),
+       Description: reader.readString(),
+        dateTime: DateTime.parse(reader.readString()),
+        isCompleted: reader.readBool(),
+        );
+   }
+     @override
+   void write(BinaryWriter writer, Todo obj){
+    writer.writeString(obj.title);
+    writer.writeString(obj.Description);
+    writer.writeString(obj.dateTime.toIso8601String());
+    writer.writeBool(obj.isCompleted);
+   }
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "todo List",
+      theme: ThemeData(
+        primaryColor: Colors.indigo
+      ),
+      home: HomePage(),
+      
+    );
+  }
+}
